cf5cd9bd90b21f266a682fb830a09963e8c3f0a5
Vinish M <vinishuchiha@gmail.com>
2023-06-16 18:14:51 +0530

    auth file added

---
+++ auth.py
@@ -0,0 +1,16 @@
+from config import Settings, get_settings
+from fastapi.security.api_key import APIKeyHeader
+from fastapi import Security, HTTPException, Depends
+from starlette.status import HTTP_403_FORBIDDEN
+from api_key_utils import validate_key
+
+api_key_header = APIKeyHeader(name="api_key", auto_error=False)
+
+async def get_username(settings: Settings = Depends(get_settings), api_key_header: str = Security(api_key_header)):
+    flag, username = validate_key(api_key_header)
+    if flag:
+        return username
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+        )
