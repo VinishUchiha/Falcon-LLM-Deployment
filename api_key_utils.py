7e527bd7e0ddbe37b25cc9fbe906a4b7d4c627cd
Vinish M <vinishuchiha@gmail.com>
2023-06-18 23:12:53 +0530

    db utils removed

--- api_key_utils.py
+++ api_key_utils.py
@@ -71,3 +71,25 @@ def count_update(username, count):
     else:
         cur.execute(f"INSERT INTO TOKEN_COUNT VALUES ('{username}', '{count}')")
         conn.commit()
+
+def create_hash_table(db_name = 'api_key.db', table_name = 'API_KEY_HASH'):
+    cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+
+    table = f""" CREATE TABLE {table_name} (
+                USER_NAME VARCHAR(255) NOT NULL,
+                HASH VARCHAR(255) NOT NULL
+            ); """
+    cur.execute(table)
+    conn.commit()
+    conn.close()
+
+def create_token_count_table(db_name = 'api_key.db', table_name = 'TOKEN_COUNT'):
+    cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+
+    table = f""" CREATE TABLE {table_name} (
+                USER_NAME VARCHAR(255) NOT NULL,
+                COUNT INT NOT NULL
+            ); """
+    cur.execute(table)
+    conn.commit()
+    conn.close()
