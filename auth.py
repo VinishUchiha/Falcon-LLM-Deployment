3eb2a83814093622ea9ed4aa680f6fac5f5d6d21
Vinish M <vinishuchiha@gmail.com>
2023-06-19 22:19:53 +0530

    auth updated

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security, HTTPException, Depends
 from starlette.status import HTTP_403_FORBIDDEN
-from api_key_utils import validate_key
+from utils import validate_key
 
 api_key_header = APIKeyHeader(name="api_key", auto_error=False)
 
